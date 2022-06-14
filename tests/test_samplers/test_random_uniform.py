0517bc2815fd2f428374a837882d35cef1ba0c3b
Marco Favorito <marco.favorito@bancaditalia.it>
2022-06-14 13:50:15 +0200

    feat: update RandomUniformSampler with new interface
    This commit makes RandomUniformSampler to use sample_batch rather than single_sample.
    This allows to use vectorized Numpy operations and improve performances at the
    increase of batch_size.

--- tests/test_samplers/test_random_uniform.py
+++ tests/test_samplers/test_random_uniform.py
@@ -21,14 +21,14 @@ from black_it.search_space import SearchSpace
 
 expected_params = np.array(
     [
-        [0.85, 0.64],
-        [0.47, 0.51],
-        [0.84, 0.26],
-        [0.81, 0.08],
-        [0.73, 0.95],
-        [0.67, 0.81],
-        [0.44, 0.54],
-        [0.95, 0.63],
+        [0.85, 0.17],
+        [0.64, 0.82],
+        [0.51, 0.65],
+        [0.27, 0.92],
+        [0.31, 0.5],
+        [0.04, 0.61],
+        [0.07, 0.98],
+        [0.01, 0.73],
     ]
 )
 
@@ -50,7 +50,15 @@ def test_random_uniform() -> None:
 def test_random_uniform_uniqueness() -> None:
     """Test the random uniform uniqueness."""
     existing_points = expected_params[:1]
-    expected_params_unique = np.vstack((np.array([[0.72, 0.33]]), expected_params[1:]))
+
+    # sampled_params_after_deduplication is the element that is resampled in place of expected_params[0].
+    # We add expected_params[0] as existing point; and since the test is deterministic,
+    # expected_params[0] will be sampled and detected as duplicate.
+    # then, sampled_params_after_deduplication will be the next element to be sampled.
+    element_after_deduplication = np.array([[0.63, 0.54]])
+    expected_params_unique = np.vstack(
+        (element_after_deduplication, expected_params[1:])
+    )
 
     sampler = RandomUniformSampler(batch_size=8, random_state=0)
 
