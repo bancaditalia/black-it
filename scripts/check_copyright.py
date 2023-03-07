4d15f2d08163c65dcd3f5b250366b49b320bdf39
Aldo <ealdoglielmo@gmail.com>
2023-03-07 17:58:44 +0100

    likelihood-loss: corrections by @marcofavoritobi

--- scripts/check_copyright.py
+++ scripts/check_copyright.py
@@ -33,7 +33,7 @@ from pathlib import Path
 HEADER_REGEX = re.compile(
     r"""(#!/usr/bin/env python3
 )?# Black-box ABM Calibration Kit \(Black-it\)
-# Copyright \(C\) 2021-2022 Banca d'Italia
+# Copyright \(C\) 2021-(2022|2023) Banca d'Italia
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
