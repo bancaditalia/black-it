34b69c4f2f6b04a9c92b237a7f37e8b92a9c8118
muxator <antonio.muci@bancaditalia.it>
2024-09-17 23:47:34 +0200

    WIP: ensure that setup.cfg (mypy.python_version) and .ruff.toml target the same python version

---
+++ scripts/check_same_python_version.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import configparser
+import pathlib
+import platform
+import sys
+
+try:
+    import tomllib
+
+    print(
+        f"tomllib found in the stdlib, using that. Since you are running python {platform.python_version()} "
+        f"maybe you could get rid of the tomli shim in this file?",
+    )
+except ImportError:
+    # please remove this and the whole tomli directory once the project starts
+    # supporting a minimal python version of 3.11.
+    import tomli as tomllib
+
+    print(
+        f"Using tomli as shim for tomllib because you are using python {platform.python_version()}.",
+    )
+
+MY_DIR = pathlib.Path(__file__).parent.resolve()
+
+
+def get_setup_cfg_py_version() -> str:
+    setup_cfg_path = (MY_DIR / ".." / "setup.cfg").resolve()
+    setup_cfg = configparser.ConfigParser()
+    setup_cfg.read(setup_cfg_path)
+    # expected to be of the form: "3.9"
+    return setup_cfg["mypy"]["python_version"]
+
+
+def get_ruff_toml_py_version() -> str:
+    ruff_toml_path = (MY_DIR / ".." / ".ruff.toml").resolve()
+    with ruff_toml_path.open("rb") as f:
+        ruff_toml = tomllib.load(f)
+    # expected to be of the form: "py39"
+    return ruff_toml["target-version"]
+
+
+setup_cfg_python_version = get_setup_cfg_py_version()
+ruff_toml_target_version = get_ruff_toml_py_version()
+
+print(f"{ruff_toml_target_version=}")  # 'py39'
+print(f"{setup_cfg_python_version=}")  # '3.9'
+
+converted = f"py{setup_cfg_python_version.replace('.', '')}"
+if converted != ruff_toml_target_version:
+    print(f"ERROR {converted=}")
+    sys.exit(1)
+
+print("OK: the versions are the same")
---
+++ scripts/tomli/README.md
@@ -0,0 +1,10 @@
+# tomli 2.2.1
+
+This is a vendorized version of tomli v2.2.1, taken from:
+https://github.com/hukkin/tomli/commit/73c3d102eb81fe0d2b87f905df4f740f8878d8da
+
+tomli is the upstream project from which the tomllib library in python 3.11
+stdlib was forked.
+
+Once the project start dropping support for python 3.10, this shim can be
+deleted.
